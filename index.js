fb5bb2a3b9d04c6bfdd611db6857dca0fa860c9e
WR <adm@hamont.com.br>
2023-08-17 15:37:59 -0300

    corrigindo upload

--- index.js
+++ index.js
@@ -8,22 +8,49 @@ const upload = multer({ dest: 'uploads/' });
 
 const GOOGLE_API_FOLDER_ID = '1BQSYFjIow2tGshoVd9bCQzBR1oO7wVIu';
 
-app.get('/', (req, res) => {
-    const html = fs.readFileSync('index.html', 'utf-8');
-    res.send(html);
-});
+app.use(express.static(__dirname));
 
-app.use(express.static(__dirname)); // Serve static files from the current directory
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.post('/upload', upload.single('image'), async (req, res) => {
     try {
-        // Rest of the upload logic remains the same
+        const auth = new google.auth.GoogleAuth({
+            keyFile: './cred.json',
+            scopes: ['https://www.googleapis.com/auth/drive']
+        });
+
+        const driveService = google.drive({
+            version: 'v3',
+            auth
+        });
+
+        const fileMetaData = {
+            name: req.file.originalname,
+            parents: [GOOGLE_API_FOLDER_ID]
+        };
+
+        const media = {
+            mimeType: req.file.mimetype,
+            body: fs.createReadStream(req.file.path)
+        };
+
+        const response = await driveService.files.create({
+            resource: fileMetaData,
+            media: media,
+            fields: 'id'
+        });
+
+        fs.unlinkSync(req.file.path); // Remove the uploaded file
+
+        res.send(`Image uploaded. File ID: ${response.data.id}`);
     } catch (err) {
         console.log('Upload file error', err);
         res.status(500).send('Error uploading image');
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(5500, () => {
+    console.log('Server is running on port 5500');
 });
